2628da0998f8f3d5b3f848ddc9f35e022ba21542
Ian Skelskey <ianskelskey@gmail.com>
2024-02-27 01:04:34 -0500

    Add controls for player paddle

---
+++ pong.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+var score := [0, 0] # 0: Player, 1: CPU
+const PADDLE_SPEED : int = 500
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
